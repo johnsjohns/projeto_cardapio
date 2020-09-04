c1bab67ee32bcc0907e56c8b8ecf704916b16c7f
johnsjohns <o-anao@hotmail.com>
2020-09-03 22:56:33 -0300

    Adicionado titulo e comecando com o css

---
+++ js/titulo.js
@@ -0,0 +1,18 @@
+let titulo = "Restaurante da Candinha"
+posicao = 0
+document.querySelector("body").onload = carrega();
+
+function carrega() {
+    setInterval(correTitulo, 300);
+}
+
+function correTitulo() {
+    document.querySelector("title").innerHTML = titulo.substring(posicao, titulo.length) + " " + titulo.substring(0, posicao);
+    posicao++;
+    if (posicao >= titulo.length) {
+        posicao = 0;
+    }
+
+    console.log(posicao);
+
+}
